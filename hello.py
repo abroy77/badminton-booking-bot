85d74d97b2cc9788280df81353abf2a963b831a6
Abhishek Roy <royabhishek77@gmail.com>
2024-11-05 16:59:22 +0000

    functioning bot!

--- hello.py
+++ hello.py
@@ -9,10 +9,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 from enum import StrEnum
 from selenium.webdriver.remote.webelement import WebElement
-from collections import namedtuple
+from datetime import time as Time
 
 
-def str_to_time(time_str: str) -> datetime.time:
+def str_to_time(time_str: str) -> Time:
     """converts a string in the format HH:MM to a datetime.time object
 
     Args:
@@ -40,44 +40,17 @@ LOGIN_URL = f"{ROOT_URL}/mrmlogin.aspx"
 HOMEPAGE_URL = f"{ROOT_URL}/memberHomePage.aspx"
 MY_BOOKINGS = f"{ROOT_URL}/mrmViewMyBookings.aspx?showOption=1"
 
-
-# BADMINTON_ACER_QUICKBOOK_ID = (
-#     "ctl00_MainContent_MostRecentBookings1_Bookings_ctl01_bookingLink"
-# )
-# BADMINTON_MAIN_QUICKBOOK_ID = (
-#     "ctl00_MainContent_MostRecentBookings1_Bookings_ctl01_bookingLink"
-# )
-
-# BADMINTON_ACER_ID = "ctl00_MainContent__advanceSearchResultsUserControl_Activities_ctrl0_lnkActivitySelect_xs"
-# # the id is affected by the window width and view of the page
-# # above you can see xs in the end. this could be lg1, xs, sm, md or others based on the window size. so maybe we will search
-# # by link text
-# BADMINTON_MAIN_ID = "ctl00_MainContent__advanceSearchResultsUserControl_Activities_ctrl1_lnkActivitySelect_xs"
+TODAY = datetime.today()
 
 BADMINTON_ACER_LINK_TEXT = "Badminton ( Acer ) 55mins"
 COURT_LINK_TEXTS = {
-    "Acer": "Badminton ( Acer ) 55mins",
-    "Main": "Badminton ( Main ) 55mins",
+    "acer": "Badminton ( Acer ) 55mins",
+    "main": "Badminton ( Main ) 55mins",
 }
 
 
-Hall = StrEnum("Hall", "ACER MAIN")
+Hall = StrEnum("Hall", "MAIN ACER")
 Availability = StrEnum("Availability", "AVAILABLE BOOKED MINE")
-
-
-# class Court:
-#     date: datetime
-#     time: datetime
-#     hall: Hall
-#     availability: Availability
-#     element: WebElement
-
-#     def __init__(self, element: WebElement):
-#         self.element = element
-#         self.date = datetime.strptime(
-#             element.find_element(By.CLASS_NAME, "date").text, "%d/%m/%Y"
-#         )
-#         self.time = datetime.strptime(element.find_element(By.CLASS_NAME, "time"))
 
 
 def read_config(config_path: Path) -> dict:
@@ -87,6 +60,8 @@ def read_config(config_path: Path) -> dict:
     assert isinstance(config, dict), "Config file is not a valid TOML file"
     return config
 
+def datetime_to_str(dt: datetime) -> str:
+    return dt.strftime("%a %d %b %H:%M")
 
 def main():
     config = read_config(Path("config.toml"))
@@ -100,77 +75,108 @@ def main():
         password=config["login-credentials"]["password"],
     )
 
-    go_to_court(driver, "Acer")
+    dates_to_book = [datetime.today() + timedelta(days=delta) for delta in range(8)]
+    # remove weekdays closer than 2 days from today
+    dates_to_book = [date for date in dates_to_book if date.weekday() >= 5 or (date - TODAY).days > 1]
+    booked_courts = get_my_bookings(driver)
+    for hall, dt in booked_courts:
+        print(f"Existing booking at {hall} court at {datetime_to_str(dt)}")
+    booked_dts = [dt for _, dt in booked_courts]
 
-    driver.get(HOMEPAGE_URL)
+    for hall in Hall:
+        go_to_court(driver, hall)
+        for date in dates_to_book:
+            navigate_to_date(driver, date)
+            court_dts = get_free_courts(driver)
+            # filter times when we already have a booking
+            court_dts = [(court, dt) for court, dt in court_dts if dt not in booked_dts]
+            # pick a court to book
+            court_to_book = pick_court_to_book(court_dts, is_weekday=date.weekday() < 5)
+            if court_to_book is None:
+                print(f"No courts available for {hall} on {datetime_to_str(date)}")
+                continue
+            book_court(driver, court_to_book[0])
+            booked_court_datetime = date.replace(hour=court_to_book[1].hour, minute=court_to_book[1].minute)
+            booked_dts.append(booked_court_datetime)
+            print(f"Booked {hall} court at {datetime_to_str(booked_court_datetime)}")
+            for booked_dt in booked_dts:
+                print(f"Existing booking at {hall} court at {datetime_to_str(booked_dt)}")
+            # navigate back to the court
+            go_to_court(driver, hall)
 
-    # for delta in range(6,7):
-    #     # navigate to 7 days later
-    #     target_date = datetime.today() + timedelta(days=delta)
-    #     is_weekday = target_date.weekday() < 5
-    #     navigate_to_date(driver, target_date=target_date)
-    #     print("navigated to date")
-    #     print(f"{target_date.date()}")
+def parse_court_name(text: str) -> Hall:
+    if "Acer" in text:
+        return Hall.ACER
+    elif "Main" in text:
+        return Hall.MAIN
+    else:
+        raise ValueError("Invalid court name")
 
-    #     # booked_courts = get_my_booked_courts(driver)
-    #     # print(booked_courts)
+def correct_booking_year(booking_date: datetime) -> datetime:
+    """Adjust the year of the booking date to be the present year.
+    If the booking month is in the past, then we know that the booking is for the next year.
+    so we add 1 to the year. Otherwise, we keep the year as the present year.
 
-    #     courts = get_free_courts(driver)
-    #     for court in courts:
-    #         print(court)
-    #     print("picked court")
-    #     print(pick_court_to_book(courts, is_weekday=is_weekday))
+    Args:
+        booking_date (datetime): datetime object of booking
+
+    Returns:
+        datetime: adjusted datetime object
+    """
     
+    if booking_date.month < TODAY.month:
+        booking_date = booking_date.replace(year=TODAY.year + 1)
+    else:
+        booking_date = booking_date.replace(year=TODAY.year)
+    return booking_date
 
+def parse_court_datetime(date_text: str, time_text: str) -> datetime:
+    time_text = time_text.split("(")[0].strip()
+    date_text = date_text.strip()
+    booking_date = datetime.strptime(f"{date_text} {time_text}", r"%a %d %b %H:%M") 
+    booking_date = correct_booking_year(booking_date)
+    return booking_date
 
-# Court = namedtuple("Court", ["date", "time", "hall", "availability", "element"])
-
-class CourtTime:
-    element: WebElement
-    time: datetime
-    def __init__(self, element: WebElement):
-        self.element = element
-        self.time = str_to_time(element.get_attribute("value"))
-    def __str__(self):
-        return f"Time: {self.time}"
-    
-def get_my_bookings(driver: WebDriver) -> list[CourtTime]:
+def get_my_bookings(driver: WebDriver) -> list[tuple[Hall, datetime]]:
     if driver.current_url != MY_BOOKINGS:
         driver.get(MY_BOOKINGS)
     data_table = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "ctl00_MainContent_rptMain_ctl01_gvBookings"))
     )
     # loop through the rows of the table
+    # we know the table has these columns:
+    # Activity, Date, Time, Site, Paid, Member, Actions.
+    # we only need the first 3
     booked_courts = []
-    for row in data_table.find_elements(By.TAG_NAME, "tr"):
-        for cell in row.find_elements(By.TAG_NAME, "td"):
-            # find the element with the class time
-            time_element = cell.find_element(By.CLASS_NAME, "time")
-            booked_courts.append(str_to_time(time_element.text))
-
-
+    table_body = data_table.find_element(By.TAG_NAME, "tbody")
+    for row in table_body.find_elements(By.TAG_NAME, "tr"):
+        columns = row.find_elements(By.TAG_NAME, "td")
+        court_name = parse_court_name(columns[0].text)
+        court_time = parse_court_datetime(columns[1].text, columns[2].text)
+        booked_courts.append((court_name, court_time))
+    # go back to the homepage
+    driver.get(HOMEPAGE_URL)
     
+    return booked_courts
 
-def get_free_courts(driver) -> list[CourtTime]: 
+def get_free_courts(driver) -> list[tuple[WebElement, datetime]]:
+    date = get_page_date(driver)
     data_table = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "ctl00_MainContent_grdResourceView"))
     )
     # find all elements with the class itemavailable
     available_courts = data_table.find_elements(By.CLASS_NAME, "itemavailable")
-    # get the item within which is an input element
-    available_courts = [
-        CourtTime(court.find_element(By.TAG_NAME, "input")) for court in available_courts
-    ]
-    # filter out the courts for which that time is already booked by me
-    booked_court_times = get_my_booked_courts(driver)
-    available_courts = [
-        court for court in available_courts if court.time not in booked_court_times
-    ]
+    court_times: list[tuple[WebElement, datetime]] = []
+    for court in available_courts:
+        inner_element = court.find_element(By.TAG_NAME, "input")
+        time_txt = inner_element.get_attribute("value").strip()
+        time = datetime.strptime(time_txt, "%H:%M")
+        dt = date.replace(hour=time.hour, minute=time.minute) 
+        court_times.append((inner_element, dt))
 
-    return available_courts
+    return court_times
 
-
-def pick_court_to_book(courts:list[CourtTime], is_weekday: bool) -> CourtTime:
+def pick_court_to_book(courts:list[tuple[WebDriver, datetime]], is_weekday: bool) -> tuple[WebDriver, datetime]:
     # assume that all the courts are on the same day
     if is_weekday:
         preferences = TIME_PREFERENCES["weekdays"]
@@ -179,31 +185,36 @@ def pick_court_to_book(courts:list[CourtTime], is_weekday: bool) -> CourtTime:
     # find the first court that matches the preferences
     for desired_time in preferences:
         for court in courts:
-            if court.time == desired_time:
+            if court[1].time() == desired_time:
 
                 return court
-        
-    
-def get_my_booked_courts(driver) -> list[datetime]:
-    data_table = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "ctl00_MainContent_grdResourceView"))
+    return None
+
+def book_court(driver: WebDriver,court: WebElement):
+    # navigate to booking page
+    court.click()
+    # get the element to book
+    book_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "ctl00_MainContent_btnBasket"))
     )
-    # find all elements with the class itemavailable
-    booked_courts = data_table.find_elements(By.CLASS_NAME, "itemofcurrentuser")
-    times = [
-        str_to_time(court.find_element(By.CLASS_NAME, "btn").text)
-        for court in booked_courts
-    ]
-    return times
+    book_button.click()
+    # check for completion
+    _ = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "bookingConfirmedContent-content"))
+    )
+    # go home
+    driver.get(HOMEPAGE_URL)
+    return
 
 
 def navigate_to_date(driver: WebDriver, target_date: datetime):
     # if we're at the right date, return
     page_date = get_page_date(driver)
     if page_date.date() == target_date.date():
+        sleep(1)
         return
     # if the date is more than 7 days in the future, raise an error
-    if (target_date - datetime.today()).days > 7:
+    if (target_date - TODAY).days > 7:
         raise ValueError("Date is more than 7 days in the future")
 
     target_date_in_future = target_date > page_date
@@ -222,16 +233,11 @@ def navigate_to_date(driver: WebDriver, target_date: datetime):
 
 
 def get_page_date(driver: WebDriver) -> datetime:
-    today = datetime.today()
     page_date_element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "ctl00_MainContent_lblCurrentNavDate"))
     )
     page_date = datetime.strptime(page_date_element.text, "%a %d %b")
-    # edge case where the date is in the next year
-    if page_date.month < today.month:
-        page_date = page_date.replace(year=today.year + 1)
-    else:
-        page_date = page_date.replace(year=today.year)
+    page_date = correct_booking_year(page_date)
     return page_date
 
 
@@ -250,6 +256,8 @@ def login(driver: WebDriver, email: str, password: str):
 
 
 def go_to_court(driver: WebDriver, hall: Hall):
+    if driver.current_url != HOMEPAGE_URL:
+        driver.get(HOMEPAGE_URL)
     element = WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.LINK_TEXT, COURT_LINK_TEXTS[hall]))
     )
